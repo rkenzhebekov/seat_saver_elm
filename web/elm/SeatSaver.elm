5e9277ca08b41833a0b3893d3bc938ca34fa72a0
Ruslan Kenzhebekov <rkenzheb@pinc.com>
2017-07-24 09:08:09 -0700

    Fetch seats via api

--- web/elm/SeatSaver.elm
+++ web/elm/SeatSaver.elm
@@ -4,13 +4,38 @@ import Html exposing (..)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 
-main : Program Never Model Msg
-main =
-  Html.beginnerProgram
-    { model = init
-    , view = view
-    , update = update
-    }
+import Http
+import Json.Decode as Decode exposing (Decoder, field, succeed)
+
+apiUrlPrefix : String
+apiUrlPrefix =
+  "http://localhost:4000/api"
+
+seatsUrl : String
+seatsUrl =
+  apiUrlPrefix ++ "/seats"
+
+
+getSeats : (Result Http.Error (List Seat) -> msg) -> String -> Cmd msg
+getSeats msg url =
+  seatListDecoder
+  |> Http.get url
+  |> Http.send msg
+
+requestSeats : Cmd Msg
+requestSeats =
+  getSeats FetchSeats seatsUrl
+
+seatListDecoder : Decoder (List Seat)
+seatListDecoder =
+    Decode.list seatDecoder
+
+seatDecoder : Decoder Seat
+seatDecoder =
+  Decode.map2 Seat
+    (field "seatNo" Decode.int)
+    (field "occupied" Decode.bool)
+
 
 
 -- MODEL
@@ -24,29 +49,19 @@ type alias Seat =
 type alias Model =
   List Seat
 
-init : Model
-init =
-  [ { seatNo = 1, occupied = False }
-  , { seatNo = 2, occupied = False }
-  , { seatNo = 3, occupied = False }
-  , { seatNo = 4, occupied = False }
-  , { seatNo = 5, occupied = False }
-  , { seatNo = 6, occupied = False }
-  , { seatNo = 7, occupied = False }
-  , { seatNo = 8, occupied = False }
-  , { seatNo = 9, occupied = False }
-  , { seatNo = 10, occupied = False }
-  , { seatNo = 11, occupied = False }
-  , { seatNo = 12, occupied = False }
-  ]
+initialModel : Model
+initialModel =
+  []
 
 
 -- UPDATE
 
 
-type Msg = Toggle Seat
+type Msg
+  = Toggle Seat
+  | FetchSeats (Result Http.Error (List Seat))
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Toggle seatToToggle ->
@@ -56,7 +71,17 @@ update msg model =
             { seatFromModel | occupied = not seatFromModel.occupied }
           else seatFromModel
       in
-        List.map updateSeat model
+        ((List.map updateSeat model), Cmd.none)
+    FetchSeats (Ok seats) ->
+      let
+        _ = Debug.log "It worked" seats
+      in
+        (seats, Cmd.none)
+    FetchSeats (Err error) ->
+      let
+        _ = Debug.log "Oops!" error
+      in
+        (model, Cmd.none)
 
 
 -- VIEW
@@ -78,4 +103,13 @@ seatItem seat =
       , onClick (Toggle seat)
       ]
       [ text (toString seat.seatNo) ]
+
+main : Program Never Model Msg
+main =
+  Html.program
+    { init = ( initialModel, (getSeats FetchSeats seatsUrl) )
+    , view = view
+    , update = update
+    , subscriptions = (\_ -> Sub.none)
+    }
 
